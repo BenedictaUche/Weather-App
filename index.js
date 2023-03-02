9176cb165085afad7464af0c00c95ae08edbc2a2
Benedicta <benedictauche.onyebuchi@gmail.com>
2023-03-02 15:28:38 +0100

    added a forcast feature

--- index.js
+++ index.js
@@ -30,6 +30,36 @@ function formatDay(timestamp) {
     return days[day];
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+
+    let forecastHTML = `<div class="row">`;
+    days.forEach(function (day) {
+        forecastHTML =
+            forecastHTML +
+            `
+      <div class="col-2 card">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+    });
+
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+    console.log(forecastHTML);
+}
+
 
 function displayTemperature(response) {
     console.log(response.data);
@@ -40,6 +70,8 @@ function displayTemperature(response) {
     let windElement = document.querySelector('#wind');
     let dateElement = document.querySelector(".date");
     let iconElement = document.querySelector('#icon');
+
+    displayForecast();
 
     celsiusTemperature = Math.round(response.data.main.temp)
 
